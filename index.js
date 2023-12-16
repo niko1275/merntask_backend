2a0ff265b9a08cb9ebfdf4b7a9f81a61ac003875
Nicolas <nicolas.ignacio.h.r@gmail.com>
2023-12-16 17:42:02 -0300

    Cambios 2.0

--- index.js
+++ index.js
@@ -33,28 +33,8 @@ app.use("/api/proyectos",proyectoRoutes);
 app.use("/api/tareas",tareaRoutes);
 const PORT = process.env.PORT || 4000;
 
-const servidor = app.listen(4000,()=>{
+app.listen(4000,()=>{
     console.log(`Servidor Corriendo En el puerto ${PORT}`);
     
 })
 
-import {Server, Socket} from 'socket.io';
-
-const io = new Server(servidor,{
-    pingTimeout:60000,cors:{
-        origin:process.env.FRONTEND_URL,
-    },
-})
-
-io.on('connection',(socket)=>{
-   
-    socket.on('proyecto',(proyecto)=>{
-        socket.join(proyecto)
-    })
-
-
-    socket.on('nueva tarea',(tarea)=>{
-        const proyecto = tarea.proyecto
-        socket.on(proyecto).emit('tarea agregada',tarea);
-    })
-})
