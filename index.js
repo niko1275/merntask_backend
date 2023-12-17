e1e2d37ce31b267da4392f741bf2ab77da0848e4
Nicolas <nicolas.ignacio.h.r@gmail.com>
2023-12-17 04:05:47 -0300

    nuevo cors

--- index.js
+++ index.js
@@ -1,39 +1,30 @@
-
-import  express, { json }  from "express";
+import express from "express";
 import connectDB from "./config/db.js";
 import dotenv from 'dotenv';
 import cors from 'cors';
 import usuarioRoutes from "./routes/usuarioRoutes.js";
 import proyectoRoutes from "./routes/proyectoRoutes.js";
 import tareaRoutes from './routes/tareaRoutes.js'
-const app =  express();
+
+const app = express();
 app.use(express.json());
 dotenv.config();
 connectDB();
 
-//Configurar Cors
-
-const whitelist = [process.env.FRONTEND_URL];
+// Configurar Cors
 const corsOptions = {
-    origin: function(origin, callback) {
-        console.log(origin);
-        if (whitelist.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(new Error("Error de Cors"));
-        }
-    }
-}
+    origin: process.env.FRONTEND_URL,
+};
+
 app.use(cors(corsOptions));
 
-//Routing 
-app.use("/api/usuarios",usuarioRoutes);
-app.use("/api/proyectos",proyectoRoutes);
-app.use("/api/tareas",tareaRoutes);
+// Routing
+app.use("/api/usuarios", usuarioRoutes);
+app.use("/api/proyectos", proyectoRoutes);
+app.use("/api/tareas", tareaRoutes);
+
 const PORT = process.env.PORT || 4000;
 
-app.listen(4000,()=>{
+app.listen(PORT, () => {
     console.log(`Servidor Corriendo En el puerto ${PORT}`);
-    
-})
-
+});
